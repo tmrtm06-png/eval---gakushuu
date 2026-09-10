ca6aba7c0664a3f8d75167b72b4a9ff5b82314f1
acg0710 <acg0710-dotcom>
2026-09-10 19:56:22 +0900

    자료구조: 2회 이상의 단어  빈도 출력

---
+++ python_DS/eval173.py
@@ -0,0 +1,29 @@
+# 빈도 dict 구축 → 2회 이상만 추출 → (-count, name) tuple 정렬로 내림차순.
+words = input().split()
+
+# 빈도 dict 구축을 위한 빈 dict
+word_frequency = {}
+
+# 한 줄로 입력받은 단어를 순회
+for word in words:
+
+    if word == " ":  # word가 공백인 경우 건너뛴다
+        continue
+    # dict 안에 단어가 없는 경우 word를 추가하고, 횟수를 + 1 카운트한다
+    word_frequency[word] = word_frequency.get(word, 0) + 1
+
+# 빈도를 내림차순 tuple로 정렬
+sorted_frequency = sorted([(-count, key) for key, count in word_frequency.items()])
+
+# 빈도가 2회 이상인 단어와 그 횟수만 필터링
+duplicates = [(freq, word)for freq, word in sorted_frequency if abs(freq) >= 2 ]
+
+# 2회 이상만 필터링 한 튜플을 순회하며, 단어와 빈도를 출력
+# 빈도 리스트 유무 확인
+if duplicates:
+    for frequency, word_key in duplicates:
+        print(f"{word_key}: {-frequency}")
+
+# 상기 코드에 걸리지 않은 경우, "중복 없음" 출력
+else:
+    print("중복 없음")
